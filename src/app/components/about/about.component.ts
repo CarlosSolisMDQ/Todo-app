8334c066663cd343d1afd32335acaeb632e1cf35
CarlosSolisMDQ <carlossolisschulz@gmail.com>
2021-11-25 17:40:46 -0300

    arreglando un link

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-about',
